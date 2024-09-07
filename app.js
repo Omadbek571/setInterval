6ede6e747cb1a49ff893396cd25e36438a1e5b84
Omadbek571 <omadbektomsk08@gmail.com>
2024-09-07 15:30:52 +0500

    .

---
+++ app.js
@@ -0,0 +1,20 @@
+// setTimeout ga misol
+
+// const btn = document.getElementById("btn")  
+
+// function funcTime() {
+//     alert("Salom")
+// }
+
+// btn && btn.addEventListener("click", function (event) {
+//     setTimeout(funcTime, 3000);  
+// })
+
+// setInterval
+
+// function funcTime() {
+//     alert("Salom")
+// }
+
+// setInterval(funcTime, 3000);
+
